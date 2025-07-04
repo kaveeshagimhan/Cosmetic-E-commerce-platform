2aa9f9b10a2ac4c1a6a2c83c3f9bcc8d99a9bf25
Kaveesha Gimhan <kaveeshagimhan20@gmail.com>
2025-07-04 15:21:13 +0530

    Add product detatils to admin page

---
+++ frontend/src/assets/sampleData.js
@@ -0,0 +1,112 @@
+export const sampleProducts = [{
+        "_id": "685d7a5b0b6b3d3408eab34c",
+        "productId": "COS001",
+        "name": "HydraGlow Moisturizer",
+        "altNames": [
+            "Glow Cream",
+            "Hydration Boost"
+        ],
+        "description": "A lightweight, hydrating moisturizer for daily use with hyaluronic acid and vitamin E.",
+        "images": [
+            "https://example.com/images/hydraglow1.jpg",
+            "https://example.com/images/hydraglow2.jpg"
+        ],
+        "labelledPrice": 3500,
+        "price": 2999,
+        "stock": 120,
+        "isAvailable": true,
+        "__v": 0
+    },
+    {
+        "_id": "685d7ab50b6b3d3408eab34e",
+        "productId": "COS002",
+        "name": "MatteFix Foundation",
+        "altNames": [
+            "Oil-Free Foundation",
+            "Matte Base"
+        ],
+        "description": "Oil-free liquid foundation that provides full coverage and a long-lasting matte finish.",
+        "images": [
+            "https://example.com/images/mattefix1.jpg"
+        ],
+        "labelledPrice": 4200,
+        "price": 3999,
+        "stock": 85,
+        "isAvailable": true,
+        "__v": 0
+    },
+    {
+        "_id": "685d7ac70b6b3d3408eab350",
+        "productId": "COS003",
+        "name": "LuxeLip Velvet Matte Lipstickzz",
+        "altNames": [
+            "Red Velvetzz",
+            "Matte Lip Colorzz"
+        ],
+        "description": "A richly pigmented matte lipstick with a smooth, velvet texture and long wear.zz",
+        "images": [
+            "https://example.com/imageszz/luxelip1.jpg"
+        ],
+        "labelledPrice": 220021,
+        "price": 199914,
+        "stock": 15,
+        "isAvailable": true,
+        "__v": 0
+    },
+    {
+        "_id": "685d7a5b0b6b3d3408eab34c",
+        "productId": "COS001z",
+        "name": "HydraGlow Moisturizer",
+        "altNames": [
+            "Glow Cream",
+            "Hydration Boost"
+        ],
+        "description": "A lightweight, hydrating moisturizer for daily use with hyaluronic acid and vitamin E.",
+        "images": [
+            "https://example.com/images/hydraglow1.jpg",
+            "https://example.com/images/hydraglow2.jpg"
+        ],
+        "labelledPrice": 3500,
+        "price": 2999,
+        "stock": 120,
+        "isAvailable": true,
+        "__v": 0
+    },
+    {
+        "_id": "685d7ab50b6b3d3408eab34e",
+        "productId": "COS002z",
+        "name": "MatteFix Foundation",
+        "altNames": [
+            "Oil-Free Foundation",
+            "Matte Base"
+        ],
+        "description": "Oil-free liquid foundation that provides full coverage and a long-lasting matte finish.",
+        "images": [
+            "https://example.com/images/mattefix1.jpg"
+        ],
+        "labelledPrice": 4200,
+        "price": 3999,
+        "stock": 85,
+        "isAvailable": true,
+        "__v": 0
+    },
+    {
+        "_id": "685d7ac70b6b3d3408eab350",
+        "productId": "COS003z",
+        "name": "LuxeLip Velvet Matte Lipstickzz",
+        "altNames": [
+            "Red Velvetzz",
+            "Matte Lip Colorzz"
+        ],
+        "description": "A richly pigmented matte lipstick with a smooth, velvet texture and long wear.zz",
+        "images": [
+            "https://example.com/imageszz/luxelip1.jpg"
+        ],
+        "labelledPrice": 220021,
+        "price": 199914,
+        "stock": 15,
+        "isAvailable": true,
+        "__v": 0
+    }
+
+]
--- frontend/src/components/admin/adminProductsPage.jsx
+++ frontend/src/components/admin/adminProductsPage.jsx
@@ -1,5 +1,55 @@
+import { useEffect, useState } from "react";
+import { sampleProducts } from "../../assets/sampleData";
+import axios from "axios";
+
 export default function AdminProductPage(){
+
+    const [products, setProducts] = useState(sampleProducts);
+    useEffect(()=>{
+        axios.get(import.meta.env.VITE_API_URL + "/api/product").then(
+            (res)=>{
+                console.log(res.data);
+                setProducts(res.data);
+            }
+        );
+    },[]);
+
     return(
-            <h1>Adimn products page</h1>
+        <div className="w-full h-full max-h-full overflow-y-scroll">
+            <table className="w-[calc(90%)] text-center">
+                <thead>
+                    <tr>
+                        <th>Product ID</th>
+                        <th>Name</th>
+                        <th>Image</th>
+                        <th>Labelled Price</th>
+                        <th>Price</th>
+                        <th>Stock</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        products.map(
+                            (item,index)=>{
+                                return(
+                                    <tr key={index}>
+                                        <td>{item.productId}</td>
+                                        <td>{item.name}</td>
+                                        <td><img src={item.image} className="w-[50px] h-[50px]"/></td>
+                                        <td>{item.labelledPrice}</td>
+                                        <td>{item.price}</td>
+                                        <td>{item.stock}</td>
+                                    </tr>
+                                )
+
+                            }
+                        )
+                    }
+                    
+                    
+                </tbody>
+            </table>
+
+        </div>
     )
 }
