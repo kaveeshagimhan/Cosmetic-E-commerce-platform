5009c72aaa2b9973463f487ab87bab662a2edba5
Kaveesha Gimhan <kaveeshagimhan20@gmail.com>
2025-07-17 20:24:52 +0530

    Get product details to product overview page

--- frontend/src/components/client/productOverview.jsx
+++ frontend/src/components/client/productOverview.jsx
@@ -1,11 +1,33 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 
 export default function ProductOverviewPage(){
     const params = useParams();
     const productId = params.id
+    const [status, setStatus] = useState("loading"); //loading, success, error
+    const [product, setProduct] = useState(null)
+
+    useEffect(
+        () =>{
+            axios.get(import.meta.env.VITE_BACKEND_URL + "/api/products/" + productId).then(
+                (response) =>{
+                    console.log(response.data)
+                    setProduct(response.data)
+                    setStatus("success")
+                }
+            ).catch(
+                (error) =>{
+                    console.log(error)
+                    setStatus("error")
+                    toast.error("Error fetching product details")
+                }
+            )
+        }
+    ,[])
     return(
         <div>
-            This is overview page for product {productId}
+            This is overview page for product {product}
         </div>
     )
 }
