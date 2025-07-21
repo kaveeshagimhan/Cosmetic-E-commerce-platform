c903c04eea6559d810ab62003f99a862027a524c
Kaveesha Gimhan <kaveeshagimhan20@gmail.com>
2025-07-21 22:22:53 +0530

    add images to product show section area

--- frontend/src/components/imagesSlider.jsx
+++ frontend/src/components/imagesSlider.jsx
@@ -3,6 +3,19 @@ export default function ImageSlider(props){
 
     return(
         <div className="w-[500px] h-[600px] bg-red-900">
+            <img className="w-full h-[500px]"/>
+            <div className="w-full h-[100px] bg-blue-500 flex justify-center items-center">
+                {
+                    images.map(
+                        (images,index)=>{
+                            return (
+                                <img key={index} className="w-[100px] h-[100px] m-2" src={images}/>
+                            )
+                        }
+                    )
+                }
+
+            </div>
 
         </div>
     )
--- frontend/src/pages/admin/productsPage.jsx
+++ frontend/src/pages/admin/productsPage.jsx
@@ -65,7 +65,7 @@ export default function AdminProductPage(){
                                         <tr key={index}>
                                             <td>{item.productId}</td>
                                             <td>{item.name}</td>
-                                            <td><img src={item.images} className="w-[50px] h-[50px]"/></td>
+                                            <td><img src={item.images?.[0]} className="w-[50px] h-[50px]"/></td>
                                             <td>{item.labelledPrice}</td>
                                             <td>{item.price}</td>
                                             <td>{item.stock}</td>
