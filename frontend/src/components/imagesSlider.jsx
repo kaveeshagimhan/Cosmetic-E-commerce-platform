25697628e93912bd5031129976502bc0bcbd1c62
Kaveesha Gimhan <kaveeshagimhan20@gmail.com>
2025-07-21 23:50:23 +0530

    costomize image show in orverview

--- frontend/src/components/imagesSlider.jsx
+++ frontend/src/components/imagesSlider.jsx
@@ -1,16 +1,23 @@
+import { useState } from "react";
+
 export default function ImageSlider(props){
     const images = props.images;
+    const [currentIndex,setCurrentIndex] = useState(0);
 
     return(
-        <div className="w-[500px] h-[600px] bg-red-900">
-            <img className="w-full h-[500px]"/>
-            <div className="w-full h-[100px] bg-blue-500 flex justify-center items-center">
+        <div className="w-[500px] h-[600px]">
+            <img src={images[currentIndex]} className="w-full h-[500px] object-cover rounded-3xl"/>
+            <div className="w-full h-[100px] flex justify-center items-center">
                 {
                     images.map(
                         (images,index)=>{
                             return (
-                                <img key={index} className="w-[100px] h-[100px] m-2" src={images}/>
-                            )
+                                <img key={index} className={"w-[90px] h-[90px] m-2 rounded-2xl object-cover cursor-pointer hover:border-4 hover:border-accent "+ (index== currentIndex&& "border-accent border-4")} src={images} onClick={
+                                    ()=>{
+                                        setCurrentIndex(index);
+                                    }
+                                }/>
+                            ) 
                         }
                     )
                 }
