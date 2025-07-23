5842f84e1f6d4960f434bcfea2fe1a2152038d9a
Kaveesha Gimhan <kaveeshagimhan20@gmail.com>
2025-07-23 08:04:59 +0530

    Add product details to overview page, add cart and Buy Now buttons

--- frontend/src/pages/client/productOverview.jsx
+++ frontend/src/pages/client/productOverview.jsx
@@ -37,7 +37,35 @@ export default function ProductOverviewPage(){
     
                 </div>
     
-                <div className="w-[50%] h-full bg-blue-900">
+                <div className="w-[50%] h-full flex justify-center items-center">
+                    <div className="w-[500px] h-[600px] flex flex-col items-center">
+                        <h1 className="w-full text-center text-4xl text-secondary font-semibold">{product.name}
+                            {
+                                product.altNames.map((altName,index) =>{
+                                    return (
+                                        <span key={index} className = "text-4xl text-gray-500">{" | " + altName}</span>
+                                    )
+                                })
+                            
+                            }
+                        </h1>
+                        <h1 className = "w-full text-center my-2 text-md text-gray-500 font-semibold">{product.productId}</h1>
+                        <p className = "w-full text-center  text-md text-gray-500 font-semibold">{product.description}</p>
+                        {
+                            product.labelledPrice > product.price ?
+                            <div>
+                                <span className = "text-4xl mx-4 text-gray-500 line-through">{product.labelledPrice.toFixed(2)}</span>
+                                <span className = "text-4xl mx-4 font-bold text-accent">{product.price.toFixed(2)}</span>
+
+                            </div>
+                            :<span className = "text-4xl mx-4 font-bold text-accent">{product.price.toFixed(2)}</span>
+                        }
+                        <div className = "w-full flex justify-center items-center mt-4">
+                            <button className = "w-[200px] h-[50px] mx-4 cursor-pointer bg-accent text-white rounded-2xl hover:bg-accent/80 transition-all duration-300">Add to cart</button>
+                            <button className = "w-[200px] h-[50px] mx-4 cursor-pointer bg-accent text-white rounded-2xl hover:bg-accent/80 transition-all duration-300">Buy Now</button>
+
+                        </div>
+                    </div>
     
                 </div>
                 
