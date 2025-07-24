739351301e34ec05999bc51cb4984e3f7696f01e
Kaveesha Gimhan <kaveeshagimhan20@gmail.com>
2025-07-24 23:31:04 +0530

    create cart functions

---
+++ frontend/src/utils/cart.js
@@ -0,0 +1,44 @@
+export function getCart() {
+    let cart = localStorage.getItem("cart");
+    if (cart == null) {
+        cart = [];
+        localStorage.setItem("cart", JSON.stringify(cart));
+    }
+}
+
+export function removeFromCart(productId) {
+    let cart = getCart();
+
+    const newCart = cart.filter((item) => {
+        return item.productId != productId;
+    })
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+export function addToCart(product, qty) {
+    let cart = getCart();
+
+    let index = cart.findIndex((item) => {
+        return item.productId == product.productId;
+    });
+
+    if (index == -1) {
+        cart[cart.length] = {
+            productId: product.productId,
+            name: product.name,
+            image: product.images[0],
+            price: product.price,
+            labelledPrice: product.labelledPrice,
+            qty: qty
+        }
+    } else {
+        const newQty = cart[index].qty + qty;
+        if (newQty <= 0) {
+            removeFromCart(product.productId);
+            return;
+        } else {
+            cart[index].qty = newQty;
+        }
+    }
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
