8a6342dbffc9c14648f92f7fb315f9464f31c593
Kaveesha Gimhan <kaveeshagimhan20@gmail.com>
2025-08-08 21:08:15 +0530

    update admin panel interface

--- frontend/src/components/adminPage.jsx
+++ frontend/src/components/adminPage.jsx
@@ -1,17 +1,28 @@
-import { Link, Route, Routes } from "react-router-dom";
+import { Link, Route, Routes, useLocation } from "react-router-dom";
 import AddProductPage from "../pages/admin/addProductPage";
 import EditProductPage from "../pages/admin/editProductPage";
 import AdminProductPage from "../pages/admin/productsPage";
 
 export default function AdminPage(){
+
+    const location = useLocation();
+    const path = location.pathname;
+
+    function getClass(name){
+        if(path.includes(name)){
+            return "bg-gray-700 p-4"
+        }else{
+            return "text-accent p-4"
+        }
+    }
     return(
         
         <div className= "w-full h-screen flex">
-            <div className= "w-[300px] h-full flex flex-col bg-red-900">
-                <Link to = "/admin/products">Products</Link>
-                <Link to = "/admin/users">Users</Link>
-                <Link to = "/admin/orders">Orders</Link>
-                <Link to = "/admin/reviews">Reviews</Link>
+            <div className= "w-[200px] h-full flex flex-col bg-gray-500 px-4 text-accent font-bold gap-6">
+                <Link className = {getClass("products")} to = "/admin/products">Products</Link>
+                <Link className = {getClass("users")} to = "/admin/users">Users</Link>
+                <Link className = {getClass("orders")} to = "/admin/orders">Orders</Link>
+                <Link className = {getClass("reviews")} to = "/admin/reviews">Reviews</Link>
 
             </div>
             <div className= "h-full w-[calc(100%-300px)]">
